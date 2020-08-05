9c8638561e6550f646eb2b64e05bfc94ad94f17d
Stephanie Fitzgerald <steph@magnificentunicornlabs.com>
2020-08-05 15:37:34 -0600

    Daily problems

--- practices/LC3_lengthOfLongestSubstring/index.js
+++ practices/LC3_lengthOfLongestSubstring/index.js
@@ -1,26 +1,18 @@
-// Given a string, return the length of the longest substring without
-// repeating characters.
-// --- Example
-// lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
-// lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 
-function lengthOfLongestSubstring(s) {
-  let windowCharsMap ={};
-  let windowStart = 0;
-  let maxLength = 0;
+function addTwoNumbers(l1, l2) {
 
-  for (let i = 0; i < s.length; i++){
-    const endChar = s[i];
-
-    if(windowCharsMap[endChar] >= windowStart){
-      windowStart = windowCharsMap[endChar] + 1
-    }
-
-    windowCharsMap[endChar] = i;
-    maxLength = Math.max(maxLength, i - windowStart + 1)
-  }
-
-  return maxLength;
 }
 
 module.exports = lengthOfLongestSubstring;
